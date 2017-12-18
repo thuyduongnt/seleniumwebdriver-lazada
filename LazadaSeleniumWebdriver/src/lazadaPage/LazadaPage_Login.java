a92ce8aff77e137fdece3c811dcfc7d26c4b6c50
Nguyen Thi Thuy Duong <duongnttd@outlook.com>
2017-12-18 16:54:54 +0700

    test login - done!

--- LazadaSeleniumWebdriver/src/lazadaPage/LazadaPage_Login.java
+++ LazadaSeleniumWebdriver/src/lazadaPage/LazadaPage_Login.java
@@ -1,6 +1,8 @@
 package lazadaPage;
 
 import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LazadaPage_Login {
 	WebDriver driver;
@@ -12,9 +14,18 @@ public class LazadaPage_Login {
 	By msgPassword = By.xpath("//input[@id=\"LoginForm_password\"]/following::div[@class=\"s-error msg\"]");
 	By btnLogin = By.cssSelector("button.ui-buttonCta");
 	
-	By btnFacebook = By.id("facebook-login-button");
-	By btnGoogle = By.id("google-login-button");
+	By btnFacebook = By.xpath("//div[@class='fb-wrapper']/a");
+	By txtEmailFacebook = By.id("email");
+	By txtPassFacebook = By.id("pass");
+	By btnLoginFacebook = By.id("u_0_0");
+	By btnAllowFacebook = By.cssSelector("button._51sy");
 	
+	By btnGoogle = By.xpath("//div[@class='google-wrapper']/div/a");
+	By txtEmailGoogle = By.id("identifierId");
+	By btnNextGoogle1 = By.id("identifierNext");
+	By txtPassGoogle = By.xpath("//div[@id='password']/div[1]/div/div[1]/input");
+	By btnNextGoogle2 = By.id("passwordNext");
+	By btnAllowGoogle = By.xpath("//div[@id='submit_approve_access']/div[2]");
 	
 	By toggle_logout = By.cssSelector("p.dropdown__toggle_type_text");
 	By logout = By.linkText("Đăng xuất");
@@ -31,6 +42,7 @@ public class LazadaPage_Login {
 		emailField.sendKeys(email);
 	}
 	
+	//methods: login with form login
 	public void inputPassword(String password) {
 		WebElement passwordField = driver.findElement(txtPassword);
 		passwordField.clear();
@@ -49,6 +61,90 @@ public class LazadaPage_Login {
 	public String getMessagePassword() {
 		WebElement messPassword = driver.findElement(msgPassword);
 		return messPassword.getText();
+	}
+	
+	//methods: login with facebook account
+	public void clickFacebookButton() {
+		driver.findElement(btnFacebook).click();
+	}
+	
+	public void inputEmailFacebook(String email) {
+		WebElement emailFacebookField = driver.findElement(txtEmailFacebook);
+		emailFacebookField.clear();
+		emailFacebookField.sendKeys(email);
+	}
+	
+	public void inputPassFacebook(String password) {
+		WebElement passFacebookField = driver.findElement(txtPassFacebook);
+		passFacebookField.clear();
+		passFacebookField.sendKeys(password);
+	}
+	
+	public void clickLoginFacebook() {
+		driver.findElement(btnLoginFacebook).click();
+	}
+	
+	public void waitAllowButtonFacebook() {
+		WebDriverWait waiter = new WebDriverWait(driver, 10);
+		waiter.until(ExpectedConditions.visibilityOfElementLocated(btnAllowFacebook));
+	}
+	
+	public boolean clickAllowButtonFacebook() {
+		if(driver.findElement(btnAllowFacebook).isDisplayed()) {
+			driver.findElement(btnAllowFacebook).click();
+			return true;
+		}
+		else
+			return false;
+	}
+	
+	public void waitConnectAccountFacebookToLazada(String expectedTitle) {
+		WebDriverWait waiter = new WebDriverWait(driver, 15);
+		waiter.until(ExpectedConditions.titleIs(expectedTitle));
+	}
+	
+	//methods: login with google+ account
+	public void clickGoogleButton() {
+		driver.findElement(btnGoogle).click();
+	}
+	
+	public void inputEmailGoogle(String email) {
+		WebElement emailGoogleField = driver.findElement(txtEmailGoogle);
+		emailGoogleField.clear();
+		emailGoogleField.sendKeys(email);
+	}
+	
+	public void clickNextToPasswordGoogle() {
+		driver.findElement(btnNextGoogle1).click();
+	}
+	
+	public void waitPasswordFieldVisible() {
+		WebDriverWait waiter = new WebDriverWait(driver, 15);
+		waiter.until(ExpectedConditions.visibilityOfElementLocated(txtPassGoogle));
+	}
+	
+	public void inputPasswordGoogle(String password) {
+		WebElement passGoogleField = driver.findElement(txtPassGoogle);
+		passGoogleField.clear();
+		passGoogleField.sendKeys(password);
+	}
+	
+	public void clickNextToLoginGoogle() {
+		driver.findElement(btnNextGoogle2).click();
+	}
+	
+	public boolean clickAllowButtonGoogle() {
+		if(driver.findElement(btnAllowGoogle).isDisplayed()) {
+			driver.findElement(btnAllowGoogle).click();
+			return true;
+		}
+		else
+			return false;
+	}
+	
+	public void waitConnectAccountGoogle(String expectedTitle) {
+		WebDriverWait waiter = new WebDriverWait(driver, 20);
+		waiter.until(ExpectedConditions.titleIs(expectedTitle));
 	}
 	
 	public void logoutAccount() {
